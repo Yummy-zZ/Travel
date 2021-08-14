ccae951a20971c6fcf9f3e1c74b79c931fde90cb
logic <web-yi@logicdeMacBook-Air.local>
2021-08-14 12:03:00 +0800

    pro init

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="home">
     <Header></Header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import HomeSwiper from './components/HomeSwiper'
+
 export default {
   name: 'Home',
-  components: {Header}
+  components: {Header, HomeSwiper}
 }
 </script>
 
---
+++ src/pages/home/components/HomeSwiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOptions">
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOptions: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1907/cd/cdd8448b61cd6042a3.img.jpg_350x240_9bd65569.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1907/ca/ca68bd95bc36bdba3.img.jpg_350x240_0d9e7931.jpg'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_350x240_8e61302a.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background-color #fff
+  .wrapper
+    overflow hidden
+    width 100%
+    height 0
+    height 31.25%
+    background-color #eee
+    .swiper-img
+      width 100%
+</style>
