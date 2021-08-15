94342da8017fc80ec4d2e9b714fe4b9e0eb8813f
logic <web-yi@logicdeMacBook-Air.local>
2021-08-15 09:24:06 +0800

    add city-search

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <city-header></city-header>
+    <Search></Search>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
+import Search from './components/Search'
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    Search
   }
 }
 </script>
