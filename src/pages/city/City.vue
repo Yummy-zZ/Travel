38d16cf005f71ea4d29f653d1def0c42fc2cf886
logic <web-yi@logicdeMacBook-Air.local>
2021-08-15 10:58:43 +0800

    add city-list

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,17 +2,24 @@
   <div>
     <city-header></city-header>
     <Search></Search>
+    <List></List>
+    <Alphabet></Alphabet>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
 import Search from './components/Search'
+import List from './components/List'
+import Alphabet from './components/Alphabet'
+
 export default {
   name: 'City',
   components: {
     CityHeader,
-    Search
+    Search,
+    List,
+    Alphabet
   }
 }
 </script>
---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="list">
+  <ul>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Alphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "~styles/varibles"
+.list
+  position absolute
+  display flex
+  flex-direction column
+  justify-content center
+  top 1.58rem
+  right 0
+  bottom 0
+  width: .4rem
+  .item
+    text-align center
+    line-height .4rem
+    color $bgcolor
+</style>
