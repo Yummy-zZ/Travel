6305a4f9f7b03995101987766d49f29adc8f228e
logic <web-yi@logicdeMacBook-Air.local>
2021-08-15 09:15:31 +0800

    restore city-router

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,34 +1,19 @@
 <template>
-  <div class="city">
-    城市选择
-    <router-link to="/">
-      <span class="back-home iconfont">&#xe66c;</span>
-    </router-link>
+  <div>
+    <city-header></city-header>
   </div>
 </template>
 
 <script>
+import CityHeader from './components/Header'
 export default {
-  name: 'City'
+  name: 'City',
+  components: {
+    CityHeader
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-  @import "~styles/varibles"
-  .city
-    position relative
-    overflow hidden
-    height $headerHeight
-    line-height $headerHeight
-    font-size .32rem
-    background-color $bgcolor
-    color #fff
-    text-align center
-    .back-home
-      position absolute
-      width .64rem
-      font-size .4rem
-      line-height 0.86rem
-      left 0
-      color #fff
+
 </style>
---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="city">
+    城市选择
+    <router-link to="/">
+      <span class="back-home iconfont">&#xe66c;</span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/varibles"
+  .city
+    position relative
+    overflow hidden
+    height $headerHeight
+    line-height $headerHeight
+    font-size .32rem
+    background-color $bgcolor
+    color #fff
+    text-align center
+    .back-home
+      position absolute
+      width .64rem
+      font-size .4rem
+      line-height 0.86rem
+      left 0
+      color #fff
+</style>
