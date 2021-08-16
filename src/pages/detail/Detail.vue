f21f724d765b8830f3519b1b01a4aafb08c20a54
logic <web-yi@logicdeMacBook-Air.local>
2021-08-16 18:38:47 +0800

    add detail-list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,19 +2,37 @@
   <div>
     <Header></Header>
     <Banner></Banner>
-    <div class="content"></div>
+    <div class="content">
+      <List :list="list"></List>
+    </div>
   </div>
 </template>
 
 <script>
 import Banner from './components/Banner'
 import Header from './components/Header'
+import List from './components/List'
 
 export default {
   name: 'Detail',
+  data () {
+    return {
+      list: [
+        { title: '成人票',
+          cost: '998',
+          children: [{ title: '有导游参观',
+            cost: '1998',
+            children: [{title: '有人工解说', cost: '19998'}]
+          }, {title: '无导游参观', cost: '996'}] },
+        { title: '学生票' }, { title: '儿童票' },
+        { title: '特惠票' }
+      ]
+    }
+  },
   components: {
     Header,
-    Banner
+    Banner,
+    List
   }
 }
 </script>
---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="item" v-for="(item, index) in list" :key="index">
+      <div class="item-title border-bottom">
+        <span class="iconfont item-title-icon">&#xe602;</span>
+        {{item.title}}
+        <span>¥:{{item.cost}}</span>
+      </div>
+
+      <div v-if="item.children" class="item-children">
+        <List :list="item.children"></List>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .item-title-icon
+      display inline-block
+      width .36rem
+      height .36rem
+      margin-right .1rem
+    .item-title
+      line-height .8rem
+      font-size .32rem
+      padding 0 .2rem
+    .item-children
+      padding-left .4rem
+</style>
